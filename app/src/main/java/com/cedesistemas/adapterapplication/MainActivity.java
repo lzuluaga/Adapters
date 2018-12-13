5ef61b489d8dedb7a10606f8192166206dee4973
Leidy-Zulu <leidyzulu9@gmail.com>
2018-12-12 19:58:41 -0500

    Lista de productos local

---
+++ app/src/main/java/com/cedesistemas/adapterapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.cedesistemas.adapterapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    private AdapterProducts adapterProducts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        loadAdapter();
+    }
+
+    private void loadAdapter(){
+        Product product = new Product();
+        product.setProductName("Empanada");
+        product.setProductDescription("Deliciosas empanadas a tan solo 1.000 pesitos");
+
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(product);
+        products.add(product);
+        products.add(product);
+        products.add(product);
+        products.add(product);
+        products.add(product);
+
+        adapterProducts = new AdapterProducts(products);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapterProducts);
+    }
+}
---
+++ app/src/main/java/com/cedesistemas/adapterapplication/Product.java
@@ -0,0 +1,23 @@
+package com.cedesistemas.adapterapplication;
+
+public class Product {
+
+    private String productName;
+    private String productDescription;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+}
